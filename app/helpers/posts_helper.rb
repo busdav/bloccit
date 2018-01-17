06262fb74ac67527bf93f440d60d77161f4823f5
David Buser <busdav@outlook.com>
2018-01-17 22:41:38 +0000

    Assignment 27 authorization: bug fix

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,8 +2,8 @@ module PostsHelper
   def user_is_post_user_or_admin?(post)
        current_user && (current_user == post.user || current_user.admin?)
   end
-  
-  def user_is_moderator?(post)
+
+  def user_is_moderator?
        current_user && current_user.moderator?
   end
 end
