bcd318d7def35835cc8cb9c3ab3b79d041f88cf7
David Buser <busdav@outlook.com>
2018-01-20 19:54:07 +0000

    Assignment 29 voting: completed

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -35,7 +35,6 @@ RSpec.describe Post, type: :model do
   end
 
   describe "voting" do
-# #5
     before do
       3.times { post.votes.create!(value: 1, user: user) }
       2.times { post.votes.create!(value: -1, user: user) }
@@ -63,9 +62,10 @@ RSpec.describe Post, type: :model do
         expect( post.points ).to eq(@up_votes - @down_votes)
       end
     end
+  end
 
     describe "#update_rank" do
-# #28
+
       it "calculates the correct rank" do
         post.update_rank
         expect(post.rank).to eq (post.points + (post.created_at - Time.new(1970,1,1)) / 1.day.seconds)
@@ -83,5 +83,21 @@ RSpec.describe Post, type: :model do
         expect(post.rank).to eq (old_rank - 1)
       end
     end
-  end
+
+    describe "#create_vote" do
+
+      it "sets the post up_votes to 1" do
+        expect(post.up_votes).to eq(1)
+      end
+
+      it "triggers create_vote when a post is created" do
+        post = topic.posts.new(title: RandomData.random_sentence, body: RandomData.random_sentence, user: user)
+        expect(post).to receive(:create_vote)
+        post.save
+      end
+
+      it "associates the vote with the owner of the post" do
+        expect(post.votes.first.user).to eq(post.user)
+      end
+    end
 end
