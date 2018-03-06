a9f871d52f9ba9d8c192bdba1d50b5371de36efd
David Buser <busdav@outlook.com>
2018-03-06 15:19:29 +0000

    Assignment 31 public profiles: updated following requested revision

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,13 +1,38 @@
 module UsersHelper
-    def posts_for_user?(user)
-        user.posts.any? 
+
+    def show_user_posts(user)
+        if user.posts.any?
+            "<h2>Posts</h2>".html_safe
+            # render @user.posts  
+        else
+            '<div class="media-body">'.html_safe
+                "<%= @user.name %> has not submitted any posts yet.".html_safe
+            "</div>".html_safe
+        end
+    end
+
+    def show_user_comments(user)
+        if user.comments.any?
+            "<h2>Comments</h2>".html_safe
+            render @user.comments  
+        else
+            '<div class="media-body">'.html_safe
+                "<%= @user.name %> has not submitted any comments yet.".html_safe
+            "</div>".html_safe
+        end
     end
     
-    def comments_for_user?(user)
-        user.comments.any? 
-    end
-    
-    def favorite_posts_for_user?(user)
-        user.favorite_posts.any? 
+    def show_user_favorited_posts(user)
+        if user.favorite_posts.any?
+            "<h2>Favorited Posts</h2>".html_safe
+            render @user.favorite_posts  
+        else
+            '<div class="media-body">'.html_safe
+                "<%= @user.name %> has not favorited any posts yet.".html_safe
+            "</div>".html_safe
+        end
     end    
 end
+
+
+
